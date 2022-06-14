8b7b7a142cf03631c879822a873bbaf3b9acfd0d
stephen <stevedogbey@gmail.com>
2022-06-14 08:34:05 +0000

    Arrow functions completed

---
+++ index.js
@@ -0,0 +1,18 @@
+const divide = () => {
+    let result = 2000/100;
+    return result;
+}
+
+const square = (someValue) => {
+    let result = someValue*someValue
+    return result;
+}
+square(2)
+
+const add = ( num1, num2 ) => {
+    let result;
+    result = num1 + num2;
+    return result;
+
+}
+add(3,4);
